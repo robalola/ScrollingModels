3f51fef36692fcfca86b0f682fde29806c77fb45
robalola <robin.jerke@gmail.com>
2025-05-10 11:46:06 +0200

    Added multiple Scenes

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -8,14 +8,26 @@ import Fox from "./Fox.jsx";
 import CamTest from "./CamTest.jsx";
 import JulianTest from "./julian_test.jsx";
 import { Scene } from "three";
-
+import { Canvas } from "@react-three/fiber";
 export default function Experience() {
   return (
-    <>
+    <div className="Test">
+      <Scene1 />
+
+      {/* <Fox scale={0.03} position={[-2.5, 0, 2.5]} rotation-y={0.3} />
+      <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]} />}>
+        <Hamburger scale={0.4} position-x={3} />
+      </Suspense> */}
+      <h1>{"FICK JA"} </h1>
+      <Scene2 />
+    </div>
+  );
+}
+
+function Scene1() {
+  return (
+    <Canvas shadows>
       <Perf position="top-left" />
-
-      {/* <OrbitControls makeDefault /> */}
-
       <directionalLight
         castShadow
         position={[1, 2, 3]}
@@ -23,46 +35,27 @@ export default function Experience() {
         shadow-normalBias={0.04}
       />
       <ambientLight intensity={1.5} />
-      {/* <Hamburger scale={0.35} /> */}
-      {/* <ScrollControls damping={0.2} pages={2}>
-        <JulianTest />
-      </ScrollControls> */}
-      {/* <Scene1 /> */}
-      <Scene2 />
-      <mesh
-        receiveShadow
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-      >
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
-      {/* <Fox scale={0.03} position={[-2.5, 0, 2.5]} rotation-y={0.3} />
-      <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]} />}>
-        <Hamburger scale={0.4} position-x={3} />
-      </Suspense> */}
-    </>
-  );
-}
-
-function Scene1() {
-  return (
-    <>
       <ScrollControls damping={0.2} pages={2}>
         {/* <CamTest position-y={4} /> */}
         <JulianTest />
       </ScrollControls>
-    </>
+    </Canvas>
   );
 }
 function Scene2() {
   return (
-    <>
+    <Canvas shadows>
+      <directionalLight
+        castShadow
+        position={[1, 2, 3]}
+        intensity={4.5}
+        shadow-normalBias={0.04}
+      />
+      <ambientLight intensity={1.5} />
       <ScrollControls damping={0.2} pages={2}>
         <CamTest position-y={4} />
         {/* <JulianTest /> */}
       </ScrollControls>
-    </>
+    </Canvas>
   );
 }
